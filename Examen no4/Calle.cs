50240a136213bfc8e6f47acbbd3eeee7717eacc5
MARIANA N <MARIANA N@DESKTOP-30LF15P>
2023-05-10 12:45:44 -0500

    Agregar archivos de proyecto.

---
+++ Examen no4/Calle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_PavimentacionCalles
+{
+    public class Calle
+    {
+        //Atributos de la clase
+        private string tipoDeterioro;
+        private int longitud;
+        private double tramoAfectado;
+
+        //Constructor de la clase
+        public Calle()
+        {
+            tipoDeterioro = string.Empty;
+            longitud = 0;
+            tramoAfectado = 0;
+        }
+
+        //Propiedades vinculadas a los atributos
+        public string TipoDeterioro
+        {
+            get { return tipoDeterioro; }
+            set { tipoDeterioro = value; }
+        }
+
+        public int Longitud
+        {
+            get { return longitud; }
+            set { longitud = value; }
+        }
+
+        public double TramoAfectado
+        {
+            get { return tramoAfectado; }
+            set { tramoAfectado = value; }
+        }
+
+    }
+}
---
+++ PruebasUnitarias/pruebas unitarias.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex03_PavimentacionCalles;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class pruebas_unitarias
+    {
+        [TestMethod]
+        public void TotalizaAfectacionesPorDeterioro_shouldCalcAndreturntotalafectados()
+        {
+            string[] losDeterioros = { "Agrietamientos", "Hundimientos", "Ondulaciones" };
+            Calle[] calles = new Calle[2];
+            calles[0]=new Calle();
+            calles[0].TipoDeterioro = losDeterioros[0];
+            calles[0].Longitud = 100;
+            calles[0].TramoAfectado = 20;
+
+            calles[1] = new Calle();
+            calles[1].TipoDeterioro = losDeterioros[2];
+            calles[1].Longitud = 100;
+            calles[1].TramoAfectado = 20;
+            int[] respuesta = Ex03_PavimentacionCalles.Program.TotalizaAfectacionesPorDeterioro(calles, losDeterioros);
+            CollectionAssert.AreEqual(new int[] { 1, 0, 1 }, respuesta); 
+        }
+        [TestMethod]
+        public void ObtieneLongitudPromedioTramosPorDeterioro_shouldCalcAndreturntramospordeterioro()
+        {
+
+            string[] losDeterioros = { "Agrietamientos", "Hundimientos", "Ondulaciones" };
+            Calle[] calles = new Calle[2];
+            calles[0] = new Calle();
+            calles[0].TipoDeterioro = losDeterioros[0];
+            calles[0].Longitud = 100;
+            calles[0].TramoAfectado = 20;
+       
+            calles[1] = new Calle();
+            calles[1].TipoDeterioro = losDeterioros[0];
+            calles[1].Longitud = 500;
+            calles[1].TramoAfectado = 210;
+            double[] respuesta = Ex03_PavimentacionCalles.Program.ObtieneLongitudPromedioTramosPorDeterioro(calles, losDeterioros);
+            double[] esperando = new double[3]; esperando[0] = 535;
+            Assert.AreEqual(535, respuesta[0]);
+
+             
+
+
+
+
+        }
+
+
+    }
+}
